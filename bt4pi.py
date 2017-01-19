a20d6c7eee914ff7679c67d2c016e5f88e2c5c21
Elliot Penn <epenn28@vt.edu>
2017-01-19 15:56:44 -0500

    Fetching hardcoded next bus data implemented

--- bt4pi.py
+++ bt4pi.py
@@ -1,4 +1,18 @@
+#!/user/bin/env python3
+
 # BT4Pi
 
 # This bot interfaces with Twitter and the BT4U WebService API
 # to automatically reply to direct messages with bus data.
+
+import requests
+import xml.etree.ElementTree as ET
+
+userData = {'routeShortName': 'HWDB', 'stopCode': '1114'}
+r = requests.post("http://216.252.195.248/webservices/bt4u_webservice.asmx/GetNextDepartures", data=userData)
+root = ET.fromstring(r.text)
+
+for bus in root.findall('NextDepartures'):
+    nextbus = bus.find('AdjustedDepartureTime').text
+    print(nextbus)
+
