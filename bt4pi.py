3698a29cd1464231dbfde1fc9df433981103114b
Elliot Penn <epenn28@vt.edu>
2017-01-19 23:53:44 -0500

    Cleaned up message fetching

--- bt4pi.py
+++ bt4pi.py
@@ -12,9 +12,12 @@ from credentials import *
 api = twitter.Api(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET, access_token_key=ACCESS_TOKEN, access_token_secret=ACCESS_SECRET)
 
 def getMessage():
-    tweets = api.GetMentions()
-    for thing in tweets:
-        print (thing.user.screen_name)
-    return
+    mostRecentTweet = api.GetMentions()[0]
+    sn = mostRecentTweet.user.screen_name
+    txt = mostRecentTweet.text
+    txt = txt.split(" ", 1)
     
-getMessage()
+    return txt[1]
+    
+message = getMessage()
+print(message)
